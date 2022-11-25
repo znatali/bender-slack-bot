2c1d706b3da425935b6c7e354d253297257c94c8
Maksim Kuznetsov <maksim.kuznetsov@akvelon.com>
2022-11-25 22:21:03 +0400

    adding key-value storage

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
 from slack_bolt.async_app import AsyncApp
 
 from commands.wallpaper import wallpaper_command_handler
-from db.scheme import init_database
+from db.scheme import init_database, get_cursor
+from config import DB_PATH
 
 SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
 SIGNING_SECRET = os.environ.get('SIGNING_SECRET')
@@ -22,6 +23,56 @@ app_handler = AsyncSlackRequestHandler(app)
 async def handle_app_mentions(body, say, logger):
     logger.info(body)
     await say("What's up?")
+
+
+@app.command("/secret_add")
+async def add_secret_command(ack, respond, command):
+    await ack()
+    pair = command.get("text", "").split("=")
+
+    if len(pair) != 2:
+        await respond(f"{pair} can't be parsed. Please write: KEY=VALUE")
+    key, value = (i.strip() for i in pair)
+    if not key or not value:
+        await respond(f"{key=} {value=} can't be None")
+    cur = get_cursor(DB_PATH)
+    cur.execute("INSERT INTO SECRETS(key, value, chat_id) VALUES(?,?,?)", (key, value, command['channel_id']))
+    res = cur.rowcount
+    if res == 1:
+        text = f"{key} successfully added"
+    else:
+        text = "something went wrong!"
+    await respond(text)
+
+
+@app.command("/secret_delete")
+async def delete_secret_command(ack, respond, command):
+    await ack()
+    key = command.get("text", "").strip()
+
+    if not key:
+        await respond(f"{key=} can't be None")
+    cur = get_cursor(DB_PATH)
+    cur.execute("DELETE FROM SECRETS where key=(?) and chat_id=(?)", (key, command['channel_id']))
+    res = cur.rowcount
+    if res == 1:
+        text = f"{key} successfully deleted"
+    elif res == 0:
+        text = f"{key} not found!"
+    else:
+        text = "something went wrong!"
+    await respond(text)
+
+
+@app.command("/secret_list")
+async def delete_secret_command(ack, respond, command):
+    await ack()
+    cur = get_cursor(DB_PATH)
+    cur.execute("SELECT key, value from secrets where chat_id=(?)", (command['channel_id'],))
+    text = "\n".join([f"{key}={value}" for key, value in cur.fetchall()])
+    if not text:
+        text = "Storage is empty"
+    await respond(text)
 
 
 @app.command("/echo")
@@ -74,10 +125,10 @@ async def endpoint(req: Request):
 
 @api.get('/ping')
 def ping():
-    """Ping Pong endpoint of backend."""
+    """Ping-Pong endpoint of backend."""
     return {'message': 'pong'}
 
 
 if __name__ == "__main__":
-    init_database('db.sqlite3')
+    init_database(DB_PATH)
     uvicorn.run('app:api', host='0.0.0.0', reload=True)
---
+++ db/scheme.py
@@ -0,0 +1,33 @@
+import sqlite3
+import os
+from functools import cache
+
+SCHEME_QUERIES = [
+    """CREATE TABLE "SECRETS" ("id"	INTEGER NOT NULL UNIQUE,"key" TEXT NOT NULL UNIQUE,"value" TEXT NOT NULL,
+    "chat_id" TEXT NOT NULL,PRIMARY KEY("id" AUTOINCREMENT))
+""",
+]
+
+
+def init_database(path: str, force: bool = False):
+    if not os.path.exists(path) or not force:
+        return
+
+    db = sqlite3.connect(path)
+
+    cur = db.cursor()
+
+    for query in SCHEME_QUERIES:
+        cur.execute(query)
+    db.commit()
+    cur.close()
+    db.close()
+
+
+@cache
+def get_connection(path: str) -> sqlite3.Connection:
+    return sqlite3.connect(path)
+
+
+def get_cursor(path: str) -> sqlite3.Cursor:
+    return get_connection(path).cursor()
