669584569c9ef4477b48df61a091e51316cf3e2e
Maksim Kuznetsov <maksim.kuznetsov@akvelon.com>
2022-11-25 11:22:10 +0400

    replacing flask by fastapi

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-import slack
-from flask import Flask
-from slackeventsapi import SlackEventAdapter
 import os
+
+import slack
+import uvicorn
+from fastapi import FastAPI
+from fastapi_slackeventsapi import SlackEventManager
 
 SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
 SIGNING_SECRET = os.environ.get("SIGNING_SECRET")
 
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
+app = FastAPI()
+slack_event_manger = SlackEventManager(singing_secret=SIGNING_SECRET,
+                                       endpoint='/slack/events',
+                                       app=app)
 
 client = slack.WebClient(token=SLACK_TOKEN)
 
 
-@slack_event_adapter.on('message')
+@slack_event_manger.on('message')
 def message(payload):
     print(payload)
     event = payload.get('event', {})
@@ -24,5 +28,11 @@ def message(payload):
         client.chat_postMessage(channel=channel_id, text="Hello")
 
 
+@slack_event_manger.on('reaction_added')
+async def reaction_added(event_data):
+    emoji = event_data['event']['reaction']
+    print(emoji)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    uvicorn.run(app, host='0.0.0.0')
